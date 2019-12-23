b18eb17bac8dfc8babd4839b8bb47e361f059597
Ashish Modi <ashish.modi@rbi.co.uk>
2019-12-23 18:38:06 +0000

    Added cluster class

---
+++ src/cluster.js
@@ -0,0 +1,91 @@
+const qs = require("qs");
+
+class Cluster {
+
+  constructor(client, baseUrl, projectId) {
+    this.client_ = client;
+    this.baseUrl_ = baseUrl;
+    this.projectId_ = projectId;
+  }
+
+  async get(clustername, options) {
+    const queryString = qs.stringify(options);
+    const response = (
+      await this.client_.fetch(`${this.baseUrl_}/groups/${this.projectId_}/clusters/${clustername}?${queryString}`)
+      ).json();
+    return response;
+  }
+
+  async getAdvanceConfiguration(clustername, options) {
+    const queryString = qs.stringify(options);
+    const response = (
+      await this.client_.fetch(`${this.baseUrl_}/groups/${this.projectId_}/clusters/${clustername}/processArgs?${queryString}`)
+      ).json();
+    return response;
+  }
+  
+  async getAll(options) {
+    const queryString = qs.stringify(options);
+    const response = (
+      await this.client_.fetch(`${this.baseUrl_}/groups/${this.projectId_}/clusters?${queryString}`)
+      ).json();
+    return response;
+  }
+  
+  async delete(clustername, options) {
+    const queryString = qs.stringify(options);
+    await this.client_.fetch(`${this.baseUrl_}/groups/${this.projectId_}/clusters/${clustername}?${queryString}`, {
+      method: "DELETE"
+    });
+    return true;
+  }
+  
+  async update(clustername, body, options) {
+    const queryString = qs.stringify(options);
+    const response = (
+      await this.client_.fetch(`${this.baseUrl_}/groups/${this.projectId_}/clusters/${clustername}?${queryString}`, {
+        method: "PATCH",
+        body: JSON.stringify(body),
+        headers: { "Content-Type": "application/json" }
+      })
+      ).json();
+    return response;
+  }
+
+  async updateAdvanceConfiguration(clustername, body, options) {
+    const queryString = qs.stringify(options);
+    const response = (
+      await this.client_.fetch(`${this.baseUrl_}/groups/${this.projectId_}/clusters/${clustername}/processArgs?${queryString}`, {
+        method: "PATCH",
+        body: JSON.stringify(body),
+        headers: { "Content-Type": "application/json" }
+      })
+      ).json();
+    return response;
+  }
+
+  async testPrimaryFailOver(clustername, options) {
+    const queryString = qs.stringify(options);
+    const response = (
+      await this.client_.fetch(`${this.baseUrl_}/groups/${this.projectId_}/clusters/${clustername}/restartPrimaries?${queryString}`, {
+        method: "PATCH"
+      })
+      ).json();
+    return response;
+  }
+  
+  async create(body, options) {
+    const queryString = qs.stringify(options);
+    const response = (
+      await this.client_.fetch(`${this.baseUrl_}/groups/${this.projectId_}/clusters?${queryString}`, {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: { "Content-Type": "application/json" }
+      })
+      ).json();
+    return response;
+  }
+}
+
+module.exports = Cluster;
+
